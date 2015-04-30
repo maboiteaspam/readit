46619888a654faa6f05f5561e1b08e68d8d9b1fe
clement <maboiteaspam@wanadoo.fr>
2015-04-30 22:29:24 +0800

    many improvements

--- README.md
+++ README.md
@@ -6,26 +6,58 @@ CLI tool to read your MD files into your browser.
 
 ``` npm i readit-md -g```
 
-## Usage
+## Examples
 
 ```sh
+
     # open README.md at cwd
     readit 
     # open some.md at cwd
     readit some.md
+    readit some.md --port 64002
+    readit some.md --css /abs/path/to/css
+    readit some.md --template /abs/path/to/template
+    readit some.md --restart
+    
     # starts the server only
     readit --server --port 64000
+    
+```
+
+## Usage
+
+```sh
+
+  Usage: readit-md [mdFile]
+
+  Opens README file in your browser
+
+  Options:
+
+    -h, --help         output usage information
+    -V, --version      output the version number
+    --restart          Restart background server if any
+    --server           Starts http server
+    --port <port>      Port to listen on
+    --template <port>  jade template file to use
+    --css <port>       File path to your specific css
+
 ```
 
 ## Under the hood
 
-Under the hood, when you invoke the program, it will starts a detached webserver on port 64000.
+Under the hood, when you invoke the program, 
 
-If the server is already running, same process is re used.
+it will starts an http server on port 64000,
+and detach it s process.
 
-Then it opens your browser with a specific url to read the MD file from your local file system.
+If the server is already running, this process is re used.
 
-The webserver will remain open until you restart your computer.
+Then it opens your browser with a specific url 
+to read the MD file from your local file system.
+
+The server will remain open until you restart your computer,
+or kill it, if you wish to.
 
 The server listens on 127.0.0.1:64000.
 
--- index.js
+++ index.js
@@ -3,31 +3,81 @@
 var pkg = require('./package.json');
 
 var program = require('commander');
-var path = require('path');
+var fs = require('fs');
+var path = require('path-extra');
 var opener = require('opener');
 var express = require('express');
 var portscan = require('portscan');
 var spawn = require('child_process').spawn;
 var MD = require('github-markdown');
+var kill = require('tree-kill');
 
 
 program.version(pkg.version);
 
+
 program
   .usage('[mdFile]')
+  .option('--restart', 'Restart background server if any')
   .option('--server', 'Starts http server')
   .option('--port <port>', 'Port to listen on')
+  .option('--template <port>', 'jade template file to use')
+  .option('--css <port>', 'File path to your specific css')
   .description('Opens README file in your browser')
   ._name = pkg.name;
+
+
 program.parse(process.argv);
 
-var port = program.port || 64000;
+if(program.h){
+  program.help();
+}
+
+var pref = {
+  pid: null,
+  port: 64000,
+  css: null,
+  template: null
+};
+
+var prefFile = path.join(path.tempdir(), 'readit.json');
+
+if( fs.existsSync(prefFile)){
+  pref = JSON.parse(fs.readFileSync(prefFile));
+}
+
+program.restart = program.restart || (program.port && pref.port != program.port);
+
+if( program.port ){
+  pref.port = program.port;
+}
+
+if( program.css ){
+  pref.css = path.resolve(program.css);
+}
+
+if( program.template ){
+  pref.template = program.template;
+}
 
 if(program.server){
 
-  var assetsPath = path.dirname(require.resolve('github-markdown'))+'/assets/';
+  var port = pref.port;
+
+  var assetsPath = path.join(__dirname, '/assets/');
+  if(pref.template){
+    assetsPath = path.dirname(pref.template);
+  }
+
   var app = express();
   app.use(express.static( assetsPath ) );
+
+  if(pref.css){
+    app.get(path.basename(pref.css), function(){
+      res.send(fs.readFileSync(pref.css))
+    });
+  }
+
   app.get('/readit', function(req, res){
     var file = req.query.f;
     var config = {
@@ -36,6 +86,9 @@ if(program.server){
       template: assetsPath+'/template.jade'
     };
     new MD(config).render(function (html) {
+      if(pref.css){
+        html = html.replace(/<\/head>/, '<link href="/'+path.basename(pref.css)+'" rel="stylesheet"></head>')
+      }
       res.send(html);
     });
   });
@@ -44,7 +97,7 @@ if(program.server){
 }else{
 
   var rFile = path.join(process.cwd(), (program.args[0]||'README.md'));
-  var isPortOpen = function(then){
+  var isPortOpen = function(port,then){
     var opts = {};
     opts.findOne = true;
     opts.findActive = false;
@@ -55,28 +108,38 @@ if(program.server){
     opts.maxPort = port+1;
     portscan(port, opts);
   };
+  var startServer = function(){
+    var cmdLine = [];
+    process.argv.forEach(function(v){
+      cmdLine.push(v);
+    });
+    cmdLine.push('--server' );
+    var sProcess = spawn(cmdLine.shift(), cmdLine, { detached:true, stdio:'ignore' });
+    sProcess.unref();
+    pref.pid = sProcess.pid;
+    fs.writeFileSync( prefFile, JSON.stringify(pref,null,4) );
+  };
+  var openBrowser = function(port){
+    var browser = opener('http://localhost:'+port+'/readit?f='+rFile);
+    browser.unref();
+    browser.stdin.unref();
+    browser.stdout.unref();
+    browser.stderr.unref();
+  };
 
-  isPortOpen(function(open){
-    if(open){ // port is open
-      var cmdLine = [];
-      process.argv.forEach(function(v){
-        cmdLine.push(v);
-      });
-      cmdLine.push('--server' );
-      if(!program.port){
-        cmdLine.push('--port' );
-        cmdLine.push(port);
-      }
-      spawn(cmdLine.shift(), cmdLine, { detached:true, stdio:'ignore' }).unref();
+
+  if(program.restart){
+    if(pref.pid){
+      kill(parseInt(pref.pid), 'SIGKILL');
+      pref.pid = null;
     }
+  }
+  fs.writeFileSync( prefFile, JSON.stringify(pref,null,4) );
+  isPortOpen(pref.port,function(notOpen){
+    if(notOpen) startServer();
     setTimeout(function(){
-      var browser = opener('http://localhost:'+port+'/readit?f='+rFile);
-      browser.unref();
-      browser.stdin.unref();
-      browser.stdout.unref();
-      browser.stderr.unref();
-    },open?200:2000);
-
+      openBrowser(pref.port);
+    }, notOpen?2000:200);
   });
 
 }
